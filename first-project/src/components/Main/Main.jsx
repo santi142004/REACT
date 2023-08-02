8c6dbeb0fbc87fafc4297069d21276b1f2b0dc3b
santi142004 <santiagobarreralopez2004@gmail.com>
2023-08-02 10:06:33 -0500

    cambios en main

--- first-project/src/components/Main/Main.jsx
+++ first-project/src/components/Main/Main.jsx
@@ -7,25 +7,26 @@ const facebook= 'https://cdn-icons-png.flaticon.com/512/20/20673.png'
 export const Main = () => {
     return (
         <Section>
-        <section>
+        <section className='texts'>
                 <div className='text'>
                     <h3>Hello, it´s Me</h3>
                     <h2>Michael Andres Diaz</h2>
                     <h3>And I´m a <span>FrontEnd |</span></h3> <br />
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolore rerum, voluptatem  commodi autem <br /> cum labore?  Natus excepturi laborum aspernatur nesciunt. Alias odio dolorum      provident ipsa ad ab distin</p>
-                </div>
-                <div className='photo'>
-                    <img src={img} alt="hola" />
-                </div>  
-        </section>
-        <section>
-        <div className="icons">
+                <div className="icons">
                     <img src={facebook} alt="" />
                 </div>
+
                 <div className="button">
 
                 </div>
+                    
+                </div>  
+                <div className='photo'>
+                    <img src={img} alt="hola" />
+                </div>
         </section>
+
         </Section>
     )
 }
