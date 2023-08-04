8e52ac5547fc3901c0b8e9044ae04975a265e36c
santi142004 <santiagobarreralopez2004@gmail.com>
2023-08-04 08:24:13 -0500

    tailwild

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Header from './components/Header/Header'
 import { Main } from './components/Main/Main'
+import { Product } from './components/pages/Product/Product'
 
 function App() {
   return (
-    <>
-    <Header />
-    <Main />
-    </>
+    <div>
+    {/* <Header />
+    <Main /> */}
+    <Product/>
+
+    </div>
   )
 }
 
